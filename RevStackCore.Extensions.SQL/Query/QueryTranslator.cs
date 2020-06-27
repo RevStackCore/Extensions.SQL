4f14fe2168cd06d8966ab6bcb03a35af0ed619ba
meanstream <david@misonline.biz>
2020-06-27 13:16:23 -0400

    Added ColumnAttribute property support

--- RevStackCore.Extensions.SQL/Query/QueryTranslator.cs
+++ RevStackCore.Extensions.SQL/Query/QueryTranslator.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
-using RevStackCore.DataAnnotations.DataAnnotations;
+using RevStackCore.DataAnnotations;
 
 namespace RevStackCore.Extensions.SQL
 {
